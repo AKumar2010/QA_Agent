ffa7c092eac1c9a0d76f834553310b47f5e9c9e9
Abhi <abhi@Abhis-MacBook-Air.local>
2026-04-02 14:53:16 +0530

    modified logic

---
+++ agent.py
@@ -0,0 +1,34 @@
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.prompts import AIMessagePromptTemplate, PromptTemplate
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import os
+
+def get_llm_output():
+    load_dotenv()
+    #os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
+
+    # messages=[("system","Assume You are having knowledge on all the things."),
+    #           ("human","Write me a paragraph on Iran War.."),]
+    messages=[SystemMessage(content="Assume You are having knowledge on all the things."),
+              HumanMessage(content="Write me a paragraph on 1857 war..")]
+    llm=ChatOpenAI(model='gpt-5.4-mini',temperature=0.6)
+    #print(llm.invoke(messages))
+    #llm.stream(messages)
+    # output=llm.invoke(messages).content
+    # print(output)
+
+
+    prompt= PromptTemplate.from_template("give information about {topic}")
+    usrinput= input("enter your topic..")
+    myprompt=prompt.invoke({"topic":usrinput})
+    print(myprompt)
+    info=llm.invoke(myprompt)
+    print(info)
+
+    print("end of line")
+
+
+if __name__=="__main__":
+    get_llm_output()
+    
